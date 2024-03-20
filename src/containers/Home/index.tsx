520bcc26620e1f3a818e7393e1bf5b4effc635c2
david-zealinno <davidvdtak@zaronology.com>
2024-03-20 19:01:44 -0400

    updates

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import styles from './styles.module.scss';
 import Tree from "../../components/svg/Tree";
+import { motion } from "framer-motion";
 
 const Home = () => {
 
@@ -29,27 +30,27 @@ const Home = () => {
         <div className="container pb-4">
           <h2 className="mb-4 text-center">Skills</h2>    
           <div className="badgeWrap">          
-            <span>Node.js</span>
-            <span>TypeScript</span>
-            <span>React</span>
-            <span>NestJS</span>
-            <span>NextJS</span>          
-            <span>PHP</span> 
-            <span>PHP Laravel</span>         
-            <span>WordPress</span>                    
-            <span>jQuery</span>          
-            <span>CSS</span>
-            <span>MySQL</span>
-            <span>AJAX</span>
-            <span>HTML5</span>          
-            <span>CSS</span>
-            <span>Javascript</span>
-            <span>Bootstrap</span>
-            <span>JavaScript</span>
-            <span>APIs</span>          
-            <span>JSON</span>                              
-            <span>REST</span>          
-            <span>Git</span>          
+            <Badge>Node.js</Badge>
+            <Badge>TypeScript</Badge>
+            <Badge>React</Badge>
+            <Badge>NestJS</Badge>
+            <Badge>NextJS</Badge>          
+            <Badge>PHP</Badge> 
+            <Badge>PHP Laravel</Badge>         
+            <Badge>WordPress</Badge>                    
+            <Badge>jQuery</Badge>          
+            <Badge>CSS</Badge>
+            <Badge>MySQL</Badge>
+            <Badge>AJAX</Badge>
+            <Badge>HTML5</Badge>          
+            <Badge>CSS</Badge>
+            <Badge>Javascript</Badge>
+            <Badge>Bootstrap</Badge>
+            <Badge>JavaScript</Badge>
+            <Badge>APIs</Badge>          
+            <Badge>JSON</Badge>                              
+            <Badge>REST</Badge>          
+            <Badge>Git</Badge>          
           </div>
         </div>
       </div>
@@ -57,19 +58,19 @@ const Home = () => {
         <div className="container pt-4">
           <h2>Selection of API integrations</h2>    
           <div className="badgeWrap"> 
-            <span>Stripe</span>
-            <span>PayPal</span>
-            <span>Shopify</span>
-            <span>Konnektive</span>
-            <span>Sticky.io</span>
-            <span>Everflow</span>
-            <span>Facebook</span>
-            <span>Calendly</span>
-            <span>HubSpot</span>
-            <span>TicketSocket</span>
-            <span>Eventbrite</span>
-            <span>Jira</span>
-            <span>Slack</span>          
+            <Badge>Stripe</Badge>
+            <Badge>PayPal</Badge>
+            <Badge>Shopify</Badge>
+            <Badge>Konnektive</Badge>
+            <Badge>Sticky.io</Badge>
+            <Badge>Everflow</Badge>
+            <Badge>Facebook</Badge>
+            <Badge>Calendly</Badge>
+            <Badge>HubSpot</Badge>
+            <Badge>TicketSocket</Badge>
+            <Badge>Eventbrite</Badge>
+            <Badge>Jira</Badge>
+            <Badge>Slack</Badge>          
           </div>
         </div>   
       </div> 
@@ -78,3 +79,18 @@ const Home = () => {
 };
 
 export default Home;
+
+
+
+const Badge = ({ children } :  {children: any}) => {
+  return (
+    <motion.span
+      layout
+      initial={{ opacity: 0, scale: 0.2 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 1 }}
+    >
+      {children}      
+    </motion.span>
+  );
+}
